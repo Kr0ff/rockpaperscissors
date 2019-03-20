468515881c9978a70c60d123c7af50ad9ada92aa
Kr0ff <35173514+Kr0ff@users.noreply.github.com>
2019-03-20 15:19:50 +0000

    Create rps.cpp

---
+++ rps.cpp
@@ -0,0 +1,61 @@
+/*
+
+Compile: g++ rps.cpp 
+
+*/
+
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+
+void rps() {
+  
+  srand(time(NULL));
+	int computer = rand() % 3 + 1;
+	int user;
+  //int s;
+  
+  std::cout << "====================\n";
+	std::cout << "Rock Paper Scissors!\n";
+	std::cout << "====================\n";
+
+	std::cout << "1) ✊\n";
+	std::cout << "2) ✋\n";
+	std::cout << "3) ✌️\n";
+
+	std::cout << "Choice: ";
+  std::cin >> user;
+  std::cout << "\n";
+  
+  while (user == computer) {
+    std::cout << "Tie !";
+    std::cout << "\n";
+    usleep(2500000);
+    return rps();
+    break;
+  }
+  
+  if (user < computer) {
+    std::cout << "You Lose !\n";
+    usleep(1000000);
+   
+  }
+  
+  else if (user > computer) {
+    std::cout << "You Win !\n"; 
+    usleep(1000000);
+  }
+  
+  else {
+    std::cout << "Sorry ! I'm confused, try again !\n";
+    usleep(3000000);
+   // std::chrono::seconds dura(5);
+    rps();
+  }
+}
+
+int main() {
+	rps();
+}
